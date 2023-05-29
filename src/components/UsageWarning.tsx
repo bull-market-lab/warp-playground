e597781ad4d962d6a571c660066b3a77de3a3708
llllllluc <58892938+llllllluc@users.noreply.github.com>
2023-05-29 00:24:18 -0700

    update warning

--- src/components/UsageWarning.tsx
+++ src/components/UsageWarning.tsx
@@ -20,21 +20,23 @@ export const UsageWarning = () => {
         <CardBody>
           <Stack divider={<StackDivider />} spacing="4">
             <Text>
-              Limit order will expire after a day, im working on making it live
-              longer. I am working on supporting custom expiration time now.
+              Limit order will expire after a day, I am working on supporting
+              custom expiration time now.
             </Text>
             <Text>
               Executed order will send the target token to your wallet
               automatically, but for closed order, you need to withdraw the
               source token from your warp account manually, because when you
               create a limit order you first send the from token to your warp
-              account. Be careful, because token used for limit order is not
-              locked in your warp account, it is recommended to only withdraw
-              the asset when you do not have pending jobs, otherwise those
-              pending jobs might fail. I am working on a withdraw function on
-              this UI so you do not need to interact with warp account directly.
-              Also warp team is working on a fund locker so we do not
-              accidentally withdraw the fund used for any pending jobs.
+              account, if the order is not executed the source token will stay
+              there. Be careful, because token in warp account is not locked, it
+              is recommended to only withdraw the asset when you do not have
+              pending jobs, otherwise pending jobs might fail because you
+              withdraw the token that is used for the job. I am working on a
+              withdraw function on this UI so you do not need to interact with
+              warp account directly. Also warp team is working on a fund locker
+              so we do not accidentally withdraw the fund used for any pending
+              job.
             </Text>
             <Link
               // TODO: url may change after warp comes out of beta
