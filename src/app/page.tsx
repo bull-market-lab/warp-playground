60de717d8b48c491f2ffece8398ec699caa72dfb
llllllluc <58892938+llllllluc@users.noreply.github.com>
2023-07-08 10:42:45 -0700

    nit styling update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,14 @@
 import { Metadata } from "next";
 
-import UsageWarning from "@/components/common/UsageWarning";
 import Home from "@/components/page/Home";
 
 export const metadata: Metadata = {
   title: "Warp World",
-  description: "UI to harness the power of Warp Protocol",
+  description: "UI to demonstrate the power of Warp Protocol",
 };
 
 const Page = () => {
-  return (
-    <>
-      <Home />
-      <UsageWarning />
-    </>
-  );
+  return <Home />;
 };
 
 export default Page;
